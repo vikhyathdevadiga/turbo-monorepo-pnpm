6b1f3ac09d2df601107d6aef9b01c66090403419
Vikhyath S <49331452+vikhyathdevadiga@users.noreply.github.com>
2025-06-09 15:07:02 +0530

    Update button.tsx

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -12,7 +12,7 @@ export const Button = ({ children, className, appName }: ButtonProps) => {
   return (
     <button
       className={className}
-      onClick={() => alert(`Hiey! from your ${appName} app!`)}
+      onClick={() => alert(`Hiey! from your ${appName} app!!!`)}
     >
       {children}
     </button>
